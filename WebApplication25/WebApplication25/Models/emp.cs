cdcc871414a216731f0b6d78c8d6722601e4960e
debapriya23 <debapriyaparida23@gmail.com>
2020-04-03 17:47:26 +0530

    third commit

---
+++ WebApplication25/WebApplication25/Models/emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication25.Models
+{
+    public class emp
+    {
+        public int eid{ get; set; }
+        public string ename { get; set; }
+        public string edesig { get; set; }
+    }
+}
